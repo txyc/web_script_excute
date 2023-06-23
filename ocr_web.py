86327906a8e3234353157418f4c50f10fb0d689e
txyc <965622832@qq.com>
2023-06-23 10:53:49 +0800

    完成通过cookies登陆网页后上述按图片提取信息的方法

--- ocr_web.py
+++ ocr_web.py
@@ -5,7 +5,7 @@
 # ------------------------------------------------
 # 功能说明：该类用于从指定路径中的图片上传到指定网址进行在线识图文字提取
 import os
-import yaml
+import pdb
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -28,23 +28,14 @@ class Ocr_web(object):
         return picpathlist
 
     # 登陆指定网址获取连接id
-    def login_save_session(self, login_url, username, password, yaml_path):
-        # 创建浏览器选项实例
-        # chrome_options = webdriver.ChromeOptions()
-        # 配置headless无界面浏览器模式
-        # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--disable-gpu')
-        # login_path = r'C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data'
-        # chrome_options.add_argument(r"user-data-dir={:s}".format(login_path))
-        # 使用指定的浏览器选项打开指定网页，当前选项为无界面模式
-        # browser = webdriver.Chrome(chrome_options=chrome_options)
-        # 使用指定的浏览器选项打开指定网页，当前选项为界面模式
+    def login_save_session(self, login_url, username, password):
+        # 创建浏览器实例
         browser = webdriver.Chrome()
-        print(browser.capabilities['browserVersion'])
         # 设置隐式等待时长
         browser.implicitly_wait(5)
         # 连接指定连接
         browser.get(login_url)
+        browser.delete_all_cookies()
 
         # 使用指定账户登陆指定网页
         # 指定登陆方式
@@ -58,44 +49,38 @@ class Ocr_web(object):
         login_submit = browser.find_element(By.XPATH, '//*[@id="TANGRAM__PSP_4__submit"]')
         login_submit.click()
 
-        # 获取连接session信息
-        remote_executor = browser.command_executor._url
-        session_id = browser.session_id
-        # 持久化存储
-        service = dict(executor = remote_executor,sid = session_id)
-        with open(yaml_path, 'w', encoding='utf-8') as f:
-            yaml.safe_dump(service, f)
-        return service
-
-    def test_transfer(self, web_url, yaml_path):
-        caps = {
-            "capabilities": {
-                "firstMatch": [
-                    {"browserName": "chrome"},
-                ]
-            }
-        }
-        with open(yaml_path, 'r', encoding='utf-8') as f:
-            session = yaml.safe_load(f)
-        session_id = session['sid']
-        remote_executor = session['executor']
-        driver = webdriver.Remote(remote_executor, desired_capabilities=caps)
-        driver.close()
-        driver.session_id = session_id
-        driver.get(web_url)
-        time.sleep(30)
+        # 获取连接cookies信息
+        cookies = browser.get_cookies()
+        time.sleep(8)
+        return browser, cookies
 
     # 该函数用于上传图片到指定网址进行文字提取
-    def upload_transfer_picture(self, web_url, pathlist):
+    def upload_transfer_picture(self, new_browser,cookies, web_url, pathlist):
         # 创建浏览器实例，连接指定的网址
+
         # 创建浏览器选项实例
-        chrome_options = webdriver.ChromeOptions()
-        # 打开已有的浏览器，用于调试时免登陆
-        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        new_browser = webdriver.Chrome(options=chrome_options)
+        # chrome_options = webdriver.ChromeOptions()
+        # # 打开已有的浏览器，用于调试时免登陆
+        # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        # new_browser = webdriver.Chrome(options=chrome_options)
+
+        # 添加免登陆的cookies
+        # pdb.set_trace()
+        for cookie in cookies:#列表
+            if cookie['domain'] == '.baidu.com':
+                print(cookie)
+                new_browser.add_cookie({'domain':'.baidu.com',#添加cookie
+                                    'name':cookie['name'],
+                                    'value':cookie['value'],
+                                    'path':'/',
+                                    'expires':None})
+                break
 
         # 切换到指定网址
         new_browser.get(web_url)
+        time.sleep(8)
+        
+        # 逐张识别图片
         for picpath in pathlist:
             image_local_input = new_browser.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[4]/div[2]/div/div/div[2]/div[2]/div[1]/div[3]/div[1]/label')
             image_local_input.click()
@@ -123,6 +108,6 @@ if __name__ == "__main__":
     username = "18384121601"
     password = "Tianxin_2015"
     yaml_path = './session.yaml'
-    ocr_web.login_save_session(login_url, username, password, yaml_path)
-    ocr_web.test_transfer(web_url, yaml_path)
-    # ocr_web.upload_transfer_picture(web_url, picpathlist)
+    browser, cookies = ocr_web.login_save_session(login_url, username, password)
+    print(cookies)
+    ocr_web.upload_transfer_picture(browser, cookies, web_url, picpathlist)
